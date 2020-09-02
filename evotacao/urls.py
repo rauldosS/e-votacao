b03e29f9a45e904e5dcc3c38e01abcc84a4bf68f
rauldosS <48498755+rauldosS@users.noreply.github.com>
2020-09-02 19:07:38 -0300

    update interface

--- evotacao/urls.py
+++ evotacao/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('home.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
