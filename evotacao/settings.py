61935920a3c4d3815ef12beb020d4e2be65ad8d0
rauldosS <48498755+rauldosS@users.noreply.github.com>
2020-09-22 22:18:44 -0300

    update face-detector

--- evotacao/settings.py
+++ evotacao/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'usuario',
     'home',
-    'controle'
+    'controle',
+    'face_detector'
 ]
 
 MIDDLEWARE = [
--- evotacao/urls.py
+++ evotacao/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
+from face_detector import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('face_detection/detect/', views.detect),
     path('usuarios/', include('usuario.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('controle/', include('controle.urls')),
